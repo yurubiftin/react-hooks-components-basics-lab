ca602b33b34688bba27b08f079b48ef5a9ef56b9
yurubahmed <yurubiftin2002@gmail.com>
2023-01-13 14:15:24 -0500

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -16,8 +16,14 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+// write an <About> component here //
+function About(){
+  return(
+    <div id ="about">
+      <h2>About</h2>
+    </div>
+  )
+}
 
 function App() {
   return (
@@ -25,6 +31,8 @@ function App() {
       <NavBar />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
